c5240a397c2526111978fe7d992152374cfb0e63
Thống Nguyễn <thong0128@gmail.com>
2024-03-08 16:06:14 +0700

    category list view and controller category create view and controller

---
+++ src/main/java/com/codegym/minitest2/controller/CategoryController.java
@@ -0,0 +1,43 @@
+package com.codegym.minitest2.controller;
+
+import com.codegym.minitest2.model.Category;
+import com.codegym.minitest2.service.ICategoryService;
+import com.codegym.minitest2.service.ITaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryController {
+    @Autowired
+    private ICategoryService categoryService;
+    @Autowired
+    private ITaskService taskService;
+    @GetMapping
+    public ModelAndView listTask() {
+        ModelAndView modelAndView = new ModelAndView("/category/list");
+        Iterable<Category> categories = categoryService.findAll();
+        modelAndView.addObject("categories", categories);
+        return modelAndView;
+    }
+    @GetMapping("/create")
+    public ModelAndView createForm() {
+        ModelAndView modelAndView = new ModelAndView("/category/create");
+        modelAndView.addObject("category", new Category());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute("category") Category category,
+                         RedirectAttributes redirectAttributes) {
+        categoryService.save(category);
+        redirectAttributes.addFlashAttribute("message", "Create new category successfully");
+        return "redirect:/categories";
+    }
+}
--- src/main/java/com/codegym/minitest2/controller/TaskController.java
+++ src/main/java/com/codegym/minitest2/controller/TaskController.java
@@ -8,12 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/tasks")
@@ -47,6 +46,45 @@ public class TaskController {
                          RedirectAttributes redirectAttributes) {
         taskService.save(task);
         redirectAttributes.addFlashAttribute("message", "Create new task successfully");
+        return "redirect:/tasks";
+    }
+    @GetMapping("/search")
+    public ModelAndView listTaskSearch(@RequestParam("search") Optional<String> search, Pageable pageable){
+        Page<Task> tasks;
+        if(search.isPresent()){
+            tasks = taskService.findAllByNameContaining(pageable, search.get());
+        } else {
+            tasks = taskService.findAll(pageable);
+        }
+        ModelAndView modelAndView = new ModelAndView("/task/list");
+        modelAndView.addObject("tasks", tasks);
+        return modelAndView;
+    }
+    @GetMapping("/update/{id}")
+    public ModelAndView updateForm(@PathVariable Long id) {
+        Optional<Task> task = taskService.findById(id);
+        if (task.isPresent()) {
+            ModelAndView modelAndView = new ModelAndView("/task/update");
+            modelAndView.addObject("task", task.get());
+            return modelAndView;
+        } else {
+            return new ModelAndView("/error_404");
+        }
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(@ModelAttribute("customer") Task task,
+                         RedirectAttributes redirect) {
+        taskService.save(task);
+        redirect.addFlashAttribute("message", "Update task successfully");
+        return "redirect:/tasks";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id,
+                         RedirectAttributes redirect) {
+        taskService.remove(id);
+        redirect.addFlashAttribute("message", "Delete task successfully");
         return "redirect:/tasks";
     }
 }
