e6c41a347269fef25eb089a3e83f02b1dd84e1e8
Thống Nguyễn <thong0128@gmail.com>
2024-03-08 16:36:11 +0700

    done CRUD

--- src/main/java/com/codegym/minitest2/controller/CategoryController.java
+++ src/main/java/com/codegym/minitest2/controller/CategoryController.java
@@ -1,16 +1,16 @@
 package com.codegym.minitest2.controller;
 
 import com.codegym.minitest2.model.Category;
+import com.codegym.minitest2.model.Task;
 import com.codegym.minitest2.service.ICategoryService;
 import com.codegym.minitest2.service.ITaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/categories")
@@ -38,6 +38,43 @@ public class CategoryController {
                          RedirectAttributes redirectAttributes) {
         categoryService.save(category);
         redirectAttributes.addFlashAttribute("message", "Create new category successfully");
+        return "redirect:/categories";
+    }
+    @GetMapping("/update/{id}")
+    public ModelAndView updateForm(@PathVariable Long id) {
+        Optional<Category> category = categoryService.findById(id);
+        if (category.isPresent()) {
+            ModelAndView modelAndView = new ModelAndView("/category/update");
+            modelAndView.addObject("category", category.get());
+            return modelAndView;
+        } else {
+            return new ModelAndView("/error_404");
+        }
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(@ModelAttribute("category") Category category,
+                         RedirectAttributes redirect) {
+        categoryService.save(category);
+        redirect.addFlashAttribute("message", "Update category successfully");
+        return "redirect:/categories";
+    }
+    @GetMapping("/view-category/{id}")
+    public ModelAndView viewProvince(@PathVariable("id") Long id){
+        Optional<Category> categoryOptional = categoryService.findById(id);
+        if(!categoryOptional.isPresent()){
+            return new ModelAndView("/error_404");
+        }
+        Iterable<Task> tasks = taskService.findAllByCategory(categoryOptional.get());
+        ModelAndView modelAndView = new ModelAndView("/task/list");
+        modelAndView.addObject("tasks", tasks);
+        return modelAndView;
+    }
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id,
+                         RedirectAttributes redirect) {
+        categoryService.remove(id);
+        redirect.addFlashAttribute("message", "Delete category successfully");
         return "redirect:/categories";
     }
 }
